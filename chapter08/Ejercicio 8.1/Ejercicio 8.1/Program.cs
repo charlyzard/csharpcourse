705020a4327706b75c36b5df801c1d45efd8552f
Carlos Córcoles <carloscorcolest@gmail.com>
2016-02-05 11:07:37 +0100

    Capitulo 8

---
+++ chapter08/Ejercicio 8.1/Ejercicio 8.1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char tecla;
+            Console.Write("Pulse un número : ");
+            tecla = (char)Console.Read();
+            switch (tecla)
+            {
+                case '0':
+                    Console.WriteLine("Pulsaste el 0");
+                    break;
+                case '1':
+                    Console.WriteLine("Pulsaste el 1");
+                    break;
+                case '2':
+                    Console.WriteLine("Pulsaste el 2");
+                    break;
+                case '3':
+                    Console.WriteLine("Pulsaste el 3");
+                    break;
+                case '4':
+                    Console.WriteLine("Pulsaste el 4");
+                    break;
+                case '5':
+                    Console.WriteLine("Pulsaste el 5");
+                    break;
+                case '6':
+                    Console.WriteLine("Pulsaste el 6");
+                    break;
+                case '7':
+                    Console.WriteLine("Pulsaste el 7");
+                    break;
+                case '8':
+                    Console.WriteLine("Pulsaste el 8");
+                    break;
+                case '9':
+                    Console.WriteLine("Pulsaste el 9");
+                    break;
+                default:
+                    Console.WriteLine("Tecla no permitida");
+                    break;
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ chapter08/Ejercicio 8.2/Ejercicio 8.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Cuál es la forma correcta de declarar una variable que almacene un entero en C#?");
+            Console.WriteLine("a. int 1x=10");
+            Console.WriteLine("b. int x=10");
+            Console.WriteLine("c. float x=10.0f");
+            Console.WriteLine("d. string x=\"10\"");
+            Console.WriteLine("Escoja la respuesta correcta: ");
+            char respuesta = (char)Console.Read();
+            switch (respuesta)
+            {
+                case 'a':
+                    Console.WriteLine("¡Respuesta incorrecta!");
+                    break;
+                case 'b':
+                    Console.WriteLine("¡Enhorabuena! Ganas un gallifante");
+                    break;
+                case 'c':
+                    Console.WriteLine("¡Respuesta incorrecta!");
+                    break;
+                case 'd':
+                    Console.WriteLine("¡Respuesta incorrecta!");
+                    break;
+                default:
+                    Console.WriteLine("Has elegido una respuesta que no existe");
+                    break;
+
+
+            }
+        }
+    }
+}
