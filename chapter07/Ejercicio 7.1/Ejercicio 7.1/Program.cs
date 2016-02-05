d82f157852121dc8b06d86393f8d198d5c317e28
Carlos Córcoles <carloscorcolest@gmail.com>
2016-02-05 10:38:04 +0100

    Capitulo 7

---
+++ chapter07/Ejercicio 7.1/Ejercicio 7.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x, y, z;
+
+            Console.Write("Introduzca el valor 1: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Introduzca el valor 2: ");
+            y = int.Parse(Console.ReadLine());
+            Console.Write("Introduzca el valor 3: ");
+            z = int.Parse(Console.ReadLine());
+
+
+            if (x > y)
+                if (x > z)
+                    Console.Write("El valor mayor es: {0}.", x);
+                else
+                    Console.Write("El valor mayor es: {0}.", z);
+            else if (y > z)
+                Console.Write("El valor mayor es: {0}.", y);
+            else
+                Console.Write("El valor mayor es: {0}.", z);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ chapter07/Ejercicio 7.2/Ejercicio 7.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float nota;
+            Console.Write("Introduzca la nota del examen : ");
+            nota = float.Parse(Console.ReadLine());
+
+            if (nota > 10.0f)
+                Console.WriteLine("Nota demasiado alta");
+            else if (nota >= 9.0f)
+                Console.WriteLine("Sobresaliente");
+            else if (nota >= 7.0f)
+                Console.WriteLine("Notable");
+            else if (nota >= 6.0f)
+                Console.WriteLine("Bien");
+            else if (nota >= 5.0f)
+                Console.WriteLine("Suficiente");
+            else if (nota >= 0.0f)
+                Console.WriteLine("Insuficiente");
+            else
+                Console.WriteLine("No llegas ni a un cero?");
+
+
+            Console.ReadLine();
+        }
+    }
+}
