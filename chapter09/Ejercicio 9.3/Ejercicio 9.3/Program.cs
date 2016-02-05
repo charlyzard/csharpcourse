419cb8bb41befa309d16542b031743bf7623ea66
Carlos Córcoles <carloscorcolest@gmail.com>
2016-02-05 11:45:37 +0100

    Capitulo 9

---
+++ chapter09/Ejercicio 9.3/Ejercicio 9.3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_9._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eleccion;
+            string continuar = "s";
+            Console.WriteLine("¿Cuál es la palabra clave para salir de un bucle en C#?");
+            Console.WriteLine("a.quit");
+            Console.WriteLine("b.continue");
+            Console.WriteLine("c.break");
+            Console.WriteLine("d.exit");
+
+
+            while (continuar == "s")
+            {
+                Console.Write("Elija una: ");
+                eleccion = Console.ReadLine();
+
+                if (eleccion == "c")
+                {
+                    Console.WriteLine("Enhorabuena!");
+                }
+                else if (eleccion == "q" || eleccion == "n") { Console.WriteLine("Saliendo...!"); break; }
+                else Console.WriteLine("Incorrecto!");
+
+                Console.Write("Probar de nuevo? pulse s para continuar: ");
+                continuar = Console.ReadLine().ToString();
+
+            }
+        }
+    }
+}
---
+++ chapter09/Ejercicio 9.5/Ejercicio 9.5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_9._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eleccion;
+            string continuar = "s";
+            Console.WriteLine("Cuál es la palabra clave para salir de un bucle en C#?");
+            Console.WriteLine("a.quit");
+            Console.WriteLine("b.continue");
+            Console.WriteLine("c.break");
+            Console.WriteLine("d.exit");
+
+
+            do
+            {
+                Console.Write("Elija una: ");
+                eleccion = Console.ReadLine();
+
+                if (eleccion == "c")
+                {
+                    Console.WriteLine("Enhorabuena!");
+                }
+                else if (eleccion == "q" || eleccion == "n") { Console.WriteLine("Saliendo...!"); break; }
+                else Console.WriteLine("Incorrect0!");
+
+                Console.Write("Probar de nuevo? pulse s para continuar:");
+                continuar = Console.ReadLine().ToString();
+            } while (continuar == "s");
+        }
+    }
+}
