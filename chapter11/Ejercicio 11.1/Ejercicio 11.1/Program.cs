cf49d3c97bb47537327a51db271f1864b0cc187e
Carlos Córcoles <carloscorcolest@gmail.com>
2016-02-05 13:43:23 +0100

    Capitulo 11

---
+++ chapter11/Ejercicio 11.1/Ejercicio 11.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_11._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Número {0} =  ", i + 1);
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            for (int i = 4; i >= 0; i--)
+                Console.Write("{0} ", numeros[i]);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ chapter11/Ejercicio 11.2/Ejercicio 11.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_11._2
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            ImprimeSerie();
+            Console.ReadLine();
+        }
+
+        public static void ImprimeSerie()
+        {
+            int[,] numeros = new int[5, 5];
+            int i, j;
+
+            int valor = 1;
+
+            for (i = 0; i < 5; i++) //rellenamos los valores
+                for (j = 0; j < 5; j++)
+                {
+                    numeros[i, j] = valor++;
+                }
+
+            for (i = 0; i < 5; i++) //imprimimos el array
+            { 
+                for (j = 0; j < 5; j++)
+                    Console.Write("{0}\t", numeros[i, j]);
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
